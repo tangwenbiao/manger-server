05c8202c32c1b4f95e6aa3df21de1fb070583053
tangwenbiao <tangwenbiao@sm.vvip-u.com>
2020-03-14 19:50:59 +0800

    初始化项目

---
+++ src/main/java/com/tfq/manager/ManagerApplication.java
@@ -0,0 +1,18 @@
+package com.tfq.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: TangFenQi
+ * @description:
+ * @date：2020/3/14 19:43
+ */
+@SpringBootApplication
+public class ManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ManagerApplication.class);
+  }
+
+}
---
+++ src/main/java/com/tfq/manager/web/LoginController.java
@@ -0,0 +1,13 @@
+package com.tfq.manager.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: TangFenQi
+ * @description:
+ * @date：2020/3/14 19:35
+ */
+@RestController
+public class LoginController {
+
+}
